0d320c4ef8980713746777e5be72a3d6865901c9
Makayla Bryan <makaylabryan@gmail.com>
2023-03-17 12:30:47 -0400

    fave subject in school

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower()) 
+            {
+                case "math":
+                    Console.WriteLine("Mine Two!");
+                    break;
+                case "science":
+                    Console.WriteLine("That's just the Reaction I was hoping for");
+                    break;
+                case "history":
+                    Console.WriteLine("Four Score and Seven years ago...");
+                    break;
+                case "english":
+                    Console.WriteLine("To be or not to be?");
+                    break;
+                case "languages":
+                    Console.WriteLine("Hola!");
+                    break;
+                case "lunch":
+                    Console.WriteLine("How original...");
+                    break;
+                case "PE":
+                    Console.WriteLine("I too like to meander around a racing track");
+                    break;
+                default: Console.WriteLine("Hmm, interesting. I'll have to look into that.");
+                        break;
+
+
+            }
         }
     }
 }
